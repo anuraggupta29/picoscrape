392008ff743059392523173a1f2b4a2adf0a681d
ANURAG GUPTA <anuraggupta29@outlook.com>
2020-05-18 00:38:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'picoscrape',
   packages = ['picoscrape'],
-  version = '0.1',
+  version = '1.0',
   license='MIT License',
   description = 'This library enables the user to easily scrape images from various websites like unsplash, pexels. This library is dependent on selenium and other pre-installed packages.',
   author = 'Anurag Gupta',
